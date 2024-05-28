a449dfc394cd9371fe26d19582e5ee9fe9186816
anri89 <147168510+anri89@users.noreply.github.com>
2024-05-28 23:49:43 +0100

    Added some files

---
+++ Experiment22/Rainbow2/sketch.js
@@ -0,0 +1,108 @@
+let system;
+
+function setup() {
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(mouseX, mouseY));
+}
+
+function draw() {
+  background(255, 255, 255, 25); 
+  system.origin = createVector(mouseX, mouseY); 
+  system.addParticle();
+  system.run();
+}
+
+class Particle {
+  constructor(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    
+    this.lifespan = 255;
+    this.angle = 0;
+  }
+
+  run() {
+    this.update();
+    this.display();
+  }
+
+  update() {
+    let mouse = createVector(mouseX, mouseY);
+    let dir = p5.Vector.sub(mouse, this.position);
+    dir.setMag(0.05);
+    this.acceleration = dir;
+
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+    this.angle += 0.1;
+  }
+
+  display() {
+    let hue = map(this.lifespan, 0, 255, 0, 360);
+    colorMode(HSB);
+    stroke(hue, 255, 255, this.lifespan);
+    strokeWeight(2);
+    fill(hue, 255, 255, this.lifespan);
+
+    let t = map(sin(this.angle), -1, 1, 0, 1); 
+
+    push();
+    translate(this.position.x, this.position.y);
+    drawMorphingShape(0, 0, 12, 6, t, 5);
+    pop();
+
+    colorMode(RGB); 
+  }
+
+  isDead() {
+    return this.lifespan < 0;
+  }
+}
+
+class ParticleSystem {
+  constructor(position) {
+    this.origin = position.copy();
+    this.particles = [];
+  }
+
+  addParticle() {
+    this.particles.push(new Particle(this.origin));
+  }
+
+  run() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.run();
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+}
+
+// thsi is a long function to draw a morphing shape between a circle AND a star
+function drawMorphingShape(x, y, radius1, radius2, t, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx, sy;
+
+    if (t < 0.5) {
+      let blendedRadius = lerp(radius1, radius2, t * 2); 
+      sx = x + cos(a) * blendedRadius;
+      sy = y + sin(a) * blendedRadius;
+    } else {
+      let starRadius = lerp(radius2, radius1, (t - 0.5) * 2); // porpose of this part if to find the star's interpolated radius.
+      sx = x + cos(a) * starRadius;
+      sy = y + sin(a) * starRadius;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * (starRadius / 2);
+      sy = y + sin(a + halfAngle) * (starRadius / 2);
+    }
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
